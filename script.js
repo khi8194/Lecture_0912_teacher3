2f12ff96261567fe7bcd8961ec4294b17405d156
tubi55 <tubi55@nate.com>
2024-09-12 16:41:44 +0900

    자바스크립트로 클래스명 제어하는 내장 함수

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnReset = document.querySelectorAll("button")[0];
+const btnActive = document.querySelectorAll("button")[1];
+const box = document.querySelector("#box");
+
+/*
+DomEl.classList.add('클래스명') 요소에 해당 클래스명 추가
+DomEl.classList.remove('클래스명') 요소에 해당 클래스명 제거
+DomEl.classList.remove() 요소에 있는 모든 클래스명 제거
+DomEl.classList.toggle('클래스명') 해당 클래스명이 있으면 제거하고 없으면 추가
+DomEl.classList.contains('클래스명') 해당 클래스명이 있으면 true반환 없으면 false반환
+*/
