88adb9cb1edcbfcc53ac082444d7ff32fe77af5e
tubi55 <tubi55@nate.com>
2024-09-12 17:04:44 +0900

    toggle 기능 연결

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const btnReset = document.querySelectorAll("button")[0];
 const btnActive = document.querySelectorAll("button")[1];
+const btnToggle = document.querySelectorAll("button")[2];
 const box = document.querySelector("#box");
-
 /*
 DomEl.classList.add('클래스명') 요소에 해당 클래스명 추가
 DomEl.classList.remove('클래스명') 요소에 해당 클래스명 제거
@@ -15,4 +15,7 @@ btnActive.addEventListener("click", () => {
 });
 btnReset.addEventListener("click", () => {
   box.classList.remove("on");
+});
+btnToggle.addEventListener("click", () => {
+  box.classList.toggle("on");
 });
