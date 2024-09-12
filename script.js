69cb41b2639cf6377c0ff12808afcfb2b1a638db
tubi55 <tubi55@nate.com>
2024-09-12 17:46:48 +0900

    top, bottom회전 모션 추가

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const btnToggle = document.querySelector(".btnToggle");
-const [topEl, middleEl, bottomEl] = btnToggle.querySelectorAll("div");
 
 btnToggle.addEventListener("click", () => {
   btnToggle.classList.toggle("on");
